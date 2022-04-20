f1678d0de5f2822e944dae068ae094e8908b1d2d
Eric Fithian <ef2413@columbia.edu>
2022-04-20 01:49:09 -0400

    Created a repo with a walkthrough on Axios

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+const app = express();
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+    const queryOptions = {
+        params: {
+            s: 'star wars',
+            apikey: process.env.KEY
+        }
+    };
+
+    axios.get('http://www.omdbapi.com', queryOptions)
+        .then(function (response) {
+            let context = {
+                starWarsData: response.data
+            }
+            // handle success
+            console.log(context.starWarsData.Search[0].Title)
+            res.render('index', context);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        })
+});
+
+app.listen(3000);
